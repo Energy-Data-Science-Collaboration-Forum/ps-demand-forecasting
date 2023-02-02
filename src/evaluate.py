e7b6324af035041d4601594193607aa5d3144416
Adnan Fiaz <adnan.fiaz@connectnationalgrid.onmicrosoft.com>
2023-02-02 22:19:11 +0000

    FEAT: updated evaluation to allow predictions from multiple models

--- src/evaluate.py
+++ src/evaluate.py
@@ -2,24 +2,26 @@ import pandas as pd
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error
 
 
-def evaluate_models(ps_predictions, actuals):
-    ps_predictions.name = "PS_PREDICTIONS"
-    combined = pd.concat([ps_predictions, actuals], axis=1)
-
-    combined = combined.dropna()
-
-    result = pd.DataFrame(
-        {
-            "MODEL": ["PS"],
-            "MAE": [
-                mean_absolute_error(combined["PS"], combined["PS_PREDICTIONS"]),
-            ],
-            "MAPE": [
-                mean_absolute_percentage_error(
-                    combined["PS"], combined["PS_PREDICTIONS"]
-                ),
-            ],
-        }
+def evaluate_models(predictions, actuals):
+    combined = pd.concat([predictions, actuals["PS"]], axis=1)
+    result = (
+        combined.dropna()
+        .melt(
+            id_vars=["PS"],
+            value_vars=predictions.columns,
+            value_name="PREDICTIONS",
+            var_name="MODEL",
+        )
+        .groupby("MODEL")
+        .apply(lambda df: calculate_metrics(df))
+        .reset_index()        
     )
 
     return result
+
+
+def calculate_metrics(input_data):
+    mae = mean_absolute_error(input_data["PS"], input_data["PREDICTIONS"])
+    mape = mean_absolute_percentage_error(input_data["PS"], input_data["PREDICTIONS"])
+
+    return pd.Series({"MAE":mae, "MAPE":mape})
--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -1,18 +1,34 @@
+import numpy as np
 import pandas as pd
 from pandas.testing import assert_frame_equal
 from src.evaluate import evaluate_models
 
 
-def test_evaluate_models():
-    mock_predictions = pd.Series(
-        [1, 2, 3, 4, 5, 6], index=pd.date_range("2022-08-01", "2022-08-06")
+def test_evaluate_models_one_model():
+    mock_predictions = pd.DataFrame(
+        {"GLM":[1, 2, 3, 4, 5, 6]}, index=pd.date_range("2022-08-01", "2022-08-06")
     )
     actuals = pd.DataFrame(
-        {"PS": [2] * 6}, index=pd.date_range("2022-08-01", "2022-08-06")
+        {"PS": np.arange(7,1, step=-1)}, index=pd.date_range("2022-08-01", "2022-08-06")
     )
 
     result = evaluate_models(mock_predictions, actuals)
     desired_result = pd.DataFrame(
-        {"MODEL": [ "PS"], "MAE": [1.833333], "MAPE": [0.916667]}
+        {"MODEL": ["GLM"], "MAE": [3.0], "MAPE": [0.765079]}
+    )
+    assert_frame_equal(result, desired_result)
+
+
+def test_evaluate_models_two_models():
+    mock_predictions = pd.DataFrame(
+        {"GLM":[1, 2, 3, 4, 5, 6], "DIFF":[4, 5, 3, 1, 8, 6]}, index=pd.date_range("2022-08-01", "2022-08-06")
+    )
+    actuals = pd.DataFrame(
+        {"PS": np.arange(7,1, step=-1)}, index=pd.date_range("2022-08-01", "2022-08-06")
+    )
+
+    result = evaluate_models(mock_predictions, actuals)
+    desired_result = pd.DataFrame(
+        {"MODEL": ["DIFF", "GLM"], "MAE": [3.0, 3.0], "MAPE": [0.901984, 0.765079]}
     )
     assert_frame_equal(result, desired_result)
